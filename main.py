7d57d1a5bbb109d30430263f25aa6ef71d6d6e3a
Harsha <96760251+harsha2805@users.noreply.github.com>
2023-02-09 14:48:02 +0530

    Update main.py
    The number of lines of code has been reduced and a few lines have been added to overcome the attribute error.

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 import os
+# To avoid attribute errors if you are running a Python version above 3.9.6
+import collections.abc
+c = collections
+c.abc = collections.abc
 
 from pptx import Presentation
 from pptx.util import Inches
 
-#to get the name of all files in an array
-path = 'C:/Users/harsh/PycharmProjects/pythonProject/pdftoppt/img'
-dir_list: list[str] = os.listdir(path)
+# Get the name of all files in an array
+path = '<file path which contains images>'
+dir_list = os.listdir(path)
 
-# to know the number of files
-dir_name = 'C:/Users/harsh/PycharmProjects/pythonProject/pdftoppt/img'
-a = len(os.listdir(dir_name))
+# Get the number of files
+a = len(dir_list)
 
-# coverting png to slides
-i = 0
-for i in range(a):
-    img_path: str = 'C:/Users/harsh/PycharmProjects/pythonProject/pdftoppt/img/' + dir_list[i]
+# Convert png to slides
+for filename in dir_list:
+    img_path = os.path.join(path, filename)
     prs = Presentation()
-    blank_slide_layout = prs.slide_layouts[6]
-    slide = prs.slides.add_slide(blank_slide_layout)
-    # to print  bigger ppt
-    left = top = Inches(1)
-    height = Inches(5.5)
-    pic = slide.shapes.add_picture(img_path, left, top, height=height)
-    #saving files in the name of the dir_name[i]
-    prs.save(dir_list[i] + '.pptx')
-    i = i + 1
+    slide = prs.slides.add_slide(prs.slide_layouts[6])
 
-"""    
-#to print smaller ppt
-left = top = Inches(1)
-pic = slide.shapes.add_picture(img_path, left, top)
-"""
+    left = top = Inches(1)
+    height = Inches(3.5)
+    pic = slide.shapes.add_picture(img_path, left, top, height=height)
+
+    prs.save(f'{filename}.pptx')
